29d217ec0823a88065c34a8387062942d4668d97
felipe.ramos <felipe.ramos@corp.globo.com>
2026-02-03 20:25:18 -0300

    feat: add support to change to all patterns in all banks

--- app/actions.py
+++ app/actions.py
@@ -21,14 +21,20 @@ class BehaviorController():
         self.change_mode_start = None
 
     def on_start_behaviour(self, midi_connector: MidiInOutConnector):
-        self.midi_clock.set_bpm(self.state_store.bpm)
-        self.drumbrute.change_pattern(midi_connector, self.state_store.pattern)
+        self._change_pattern(midi_connector, self.state_store.pattern)
+        self._update_bpm(self.state_store.bpm)
+        if self.state_store.playing:
+            self.drumbrute.play(midi_connector)
+        else:
+            self.drumbrute.stop(midi_connector)
 
     def previous_pattern_behaviour(self, midi_connector: MidiInOutConnector, midi_msg, delta):
         self._change_pattern(midi_connector, self.state_store.pattern - 1)
+        self._update_bpm(self.state_store.bpm)
 
     def next_pattern_behaviour(self, midi_connector: MidiInOutConnector, midi_msg, delta):
         self._change_pattern(midi_connector, self.state_store.pattern + 1)
+        self._update_bpm(self.state_store.bpm)
 
     def toggle_play_behaviour(self, midi_connector: MidiInOutConnector, midi_msg, delta):
         playing = not self.state_store.playing
@@ -50,15 +56,23 @@ class BehaviorController():
         logging.info('BPM MODE ACTIVATED')
 
     def _change_pattern(self, midi_connector: MidiInOutConnector, pattern_num: int):
-        pattern_num = min(max(0, pattern_num), self.drumbrute.max_patterns - 1)
-        if pattern_num == self.state_store.pattern:
-            return
-        self.drumbrute.change_pattern(midi_connector, pattern_num)
+        pattern_num = max(0, min(pattern_num, self._max_pattern_num() - 1))
+        drumbrute_bank = pattern_num // self.drumbrute.max_patterns
+        drumbrute_pattern = pattern_num % self.drumbrute.max_patterns
+
+        logging.info(
+            'DRUMBRUTE PATTERN:%d BANK:%d',
+            drumbrute_pattern + 1, drumbrute_bank + 1)
+
+        self.drumbrute.change_bank(midi_connector, drumbrute_bank)
+        self.drumbrute.change_pattern(midi_connector, drumbrute_pattern)
         self.state_store.set_pattern(pattern_num)
-        self._update_bpm(self.state_store.bpm)
 
     def _update_bpm(self, bpm: int):
         bpm = max(0, min(bpm, self.max_bpm))
         self.midi_clock.set_bpm(bpm)
         self.state_store.set_bpm(bpm)
         logging.info('CHANGE PATTERN:%s BPM:%d', self.state_store.pattern + 1, bpm)
+
+    def _max_pattern_num(self):
+        return self.drumbrute.max_patterns * self.drumbrute.max_banks
--- devices/drumbrute.py
+++ devices/drumbrute.py
@@ -3,6 +3,8 @@ from devices.midi_connector import MidiInOutConnector
 
 class Drumbrute():
 
+    NOTE = 0x90
+    CC = 0xB0
     PC = 0xC0
     START = 0xFA
     STOP = 0xFC
@@ -13,12 +15,19 @@ class Drumbrute():
     ):
         self.channel = channel
         self.max_patterns = 16
+        self.max_banks = 4
 
     def change_pattern(self, midi_connector: MidiInOutConnector, pattern_num: int) -> int:
         cmd = self.PC + (self.channel - 1)
         pattern_num = max(0, min(pattern_num, self.max_patterns - 1))
         midi_connector.send_message([cmd, 0, pattern_num])
         return pattern_num
+
+    def change_bank(self, midi_connector: MidiInOutConnector, bank_num: int) -> int:
+        cmd = self.CC + (self.channel - 1)
+        bank_num = max(0, min(bank_num, self.max_banks - 1))
+        midi_connector.send_message([cmd, 0, bank_num])
+        return bank_num
 
     def stop(self, midi_connector: MidiInOutConnector):
         midi_connector.send_message([self.STOP, 255, 255])
